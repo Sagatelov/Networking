e71e273b8e9d5f6d3f37525471508a031dc50554
Andrew Sagatelov <>
2023-07-26 20:45:09 +0300

    add NetworkManager Class and Posts Class

---
+++ Networking/Networking/NetworkManager.swift
@@ -0,0 +1,12 @@
+//
+//  NetworkManager.swift
+//  Networking
+//
+//  Created by Andrew Sagatelov on 26.07.2023.
+//
+
+import Foundation
+
+class NetworkManager {
+    
+}
---
+++ Networking/Networking/Posts.swift
@@ -0,0 +1,15 @@
+//
+//  Posts.swift
+//  Networking
+//
+//  Created by Andrew Sagatelov on 26.07.2023.
+//
+
+import Foundation
+
+class Posts: Codable {
+    var userId: Int
+    var id: Int
+    var title: String
+    var body: String
+}
