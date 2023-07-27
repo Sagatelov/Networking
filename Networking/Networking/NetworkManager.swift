91964698800440cdd4a0fccdfcd8d184c0255f82
Andrew Sagatelov <>
2023-07-27 19:43:09 +0300

    add function getAllPosts in NetworkManager

--- Networking/Networking/NetworkManager.swift
+++ Networking/Networking/NetworkManager.swift
@@ -17,4 +17,21 @@ class NetworkManager {
     }
     
     var url = "https://jsonplaceholder.typicode.com/"
+    
+    func getAllPosts( complition: @escaping ([Posts]) -> Void) {
+        
+        guard var urlString = URL(string: url + "posts") else { return }
+        
+        URLSession.shared.dataTask(with: urlString) { data, response, error in
+            if error != nil {
+                print("DataTask error: " + "\(String(describing: error?.localizedDescription))" + "\n" )
+            } else if let resp = response as? HTTPURLResponse,
+                      resp.statusCode == 200, let data = data {
+                print(response)
+                let post = try? JSONDecoder().decode([Posts].self, from: data)
+                
+                complition(post ?? [])
+            }
+        }
+    }
 }
